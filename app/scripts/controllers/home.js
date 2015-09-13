cf12f7fd864685e215e2ddcd93a94bb588a8e9ca
Nika Miller <miller.nika@gmail.com>
2015-09-13 11:11:24 -0600

    added comments.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,6 +3,7 @@
 angular.module('home', []).controller('HomeController', function($scope) {
   $scope.colorInput = '';
 
+  // Set background/font color using ngStyle
   $scope.changeStyles = function(colorInput){
     $scope.borderColor = { 'background-color': colorInput };
     $scope.fontColor = { color: colorInput };
--- app/scripts/directives/color_parser.js
+++ app/scripts/directives/color_parser.js
@@ -5,10 +5,13 @@ angular.module('directives', ['underscore']).directive('colorParser', function($
     require: 'ngModel',
     link: function (scope, element, attrs, ngModel) {
       scope.eggClasses = ['cn', 'kitteh'];
+
+      // Check user input against our easter eggs
       scope.knownEgg = function(userInput) {
         return ((userInput.match(/(chuck\s*norris)/i)) ||
         (userInput.match(/(donald\s*trump)/i)) || (userInput.match(/(kitteh)/i)))  ? true : false;
       };
+      // Apply special egg styling
       scope.applyEgg = function(userInput) {
         if (userInput.match(/(chuck\s*norris)/i)){
           angular.element(document.querySelector('.color-display')).toggleClass('cn');
@@ -25,6 +28,8 @@ angular.module('directives', ['underscore']).directive('colorParser', function($
         if (scope.knownEgg(ngModel.$modelValue)){
           scope.applyEgg(ngModel.$modelValue);
         } else {
+          // Toggle special styling if user has changed the input and
+          // it's no longer matching any of the set easter eggs
           _.each(scope.eggClasses, function(eggClass){
             if (angular.element(document.querySelector('.color-display')).hasClass(eggClass)) {
               angular.element(document.querySelector('.color-display')).toggleClass(eggClass);
